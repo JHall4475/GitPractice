b89aa898f24ebfcdde87011a703e3a8e34f24cbc
Jared Hall <jaredhall@MacBook-Pro-3.local>
2018-08-21 19:37:51 -0600

    My first Change

--- practice.js
+++ practice.js
@@ -1,4 +1,7 @@
 const response = 2 * 4 ;
 
-console.log (`the response is ${ response}`);
+console.log (`the response is ${ response }`);
 
+const name = 'Jared'
+
+console.log(`my name is ${ name }`)
