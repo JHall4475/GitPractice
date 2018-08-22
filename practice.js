cd142de0136f6141d311eb1747bbcc206735e459
Jared Hall <jaredhall@MacBook-Pro-3.local>
2018-08-21 20:24:03 -0600

    day one code review

--- practice.js
+++ practice.js
@@ -1,7 +1,27 @@
-const response = 2 * 4 ;
 
-console.log (`the response is ${ response }`);
 
-const name = 'Jared'
 
-console.log(`my name is ${ name }`)
+
+//outer scope
+const name = 'jared';
+
+function getData(param1){
+    //inner scope
+    return `my name is ${param1}`
+};
+
+//dunction expression
+const getdata2 = function(param1) {
+    return `my name is ${ param1}`
+}
+
+//arrow function
+const getData3 = (param1) => {
+    
+}
+
+const data = getData()
+
+console.log (name)
+console.log('hey')
+
